f3670072723cb4158a4c707a5da54db5eca93027
kevintan <kevintanhongann@gmail.com>
2022-08-09 18:01:22 +0800

    Initial version of jhipster generated by JHipster-6.10.5 with blueprints: micronaut-1.0.2

---
+++ src/main/java/xyz/doubleedgetech/security/PasswordEncoder.java
@@ -0,0 +1,9 @@
+package xyz.doubleedgetech.security;
+
+import javax.validation.constraints.NotBlank;
+
+public interface PasswordEncoder {
+    String encode(@NotBlank String rawPassword);
+
+    boolean matches(@NotBlank String rawPassword, @NotBlank String encodedPassword);
+}
